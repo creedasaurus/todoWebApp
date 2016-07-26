b14229fa609197898b7434df3818b649c3a0d684
Creed Haymond <creedasaurus@gmail.com>
2016-07-26 15:07:57 -0600

    I added MongoDB drivers to the project to attempt just using MongoClient instead of Mongoose for the purpose of practice and learning how mongo works on it's own, sort of. Maybe later I'll try Mongoose or something.

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 console.log('Setting up server');
 // =================== Setup
 var express = require('express');
-var mongoose = require('mongoose');
+// var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
 var morgan = require('morgan');
 var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
@@ -10,7 +12,13 @@ var methodOverride = require('method-override');
 var app = express();
 
 // =================== Configuration
-mongoose.connect('mongodb://localhost/myNewDatabase');
+// mongoose.connect('mongodb://localhost/test');
+var url = 'mongodb://localhost/test';
+MongoClient.connect(url, function(err, db) {
+	assert.equal(null, err);
+	console.log("We are connected to Mongodb!");
+});
+
 
 app.use(express.static('public'));
 app.use(morgan('dev'));
@@ -22,3 +30,7 @@ app.use(methodOverride());
 var portNum = 8080;
 app.listen(portNum);
 console.log(`This app is listening on port ${portNum}`);
+
+
+
+
