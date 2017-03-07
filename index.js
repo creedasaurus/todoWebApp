262f089088b1c45418f2188259839fabe548586d
Creed Haymond <creedasaurus@gmail.com>
2017-03-06 19:01:44 -0700

    Big changes to go to Angular Material and using NPM

---
+++ index.js
@@ -0,0 +1,44 @@
+/**
+ * Created by creed on 3/6/2017.
+ */
+const hostname = '127.0.0.1';
+const port = 3000;
+
+let express = require('express');
+let fs = require('fs');
+
+let logger = require('morgan');
+let compression = require('compression');
+let favicon = require('favicon');
+let bodyParser = require('body-parser');
+
+
+let app = express();
+// app.disable('x-powered-by');
+
+app.use(logger('dev'));
+app.use(compression());
+// TODO: get favicon working
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static('view/index.html'));
+
+const server = app.listen(port, hostname, ()=>{
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
+
+function gracefullShutdown() {
+    console.log('Starting shutdown');
+    server.close(()=>{
+        console.log('Shutdown complete');
+    });
+}
+
+process.on('SIGTERM', ()=>{
+    gracefullShutdown()
+});
+
+process.on('SIGINT', ()=>{
+    gracefullShutdown()
+});
